12fb36b1c7c268d787f6b7b3a15adbc5177b3388
lajacl <lavette.clifton@gmail.com>
2025-10-01 18:35:09 -0400

    Add a day / night theme toggle button

--- lib/main.dart
+++ lib/main.dart
@@ -18,26 +18,45 @@ class FadingTextAnimation extends StatefulWidget {
 
 class _FadingTextAnimationState extends State<FadingTextAnimation> {
   bool _isVisible = true;
+  bool _isDayMode = true;
+
   void toggleVisibility() {
     setState(() {
       _isVisible = !_isVisible;
     });
   }
 
+  void _toggleMode() {
+    setState(() {
+      _isDayMode = !_isDayMode;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text('Fading Text Animation')),
-      body: Center(
-        child: AnimatedOpacity(
-          opacity: _isVisible ? 1.0 : 0.0,
-          duration: Duration(seconds: 1),
-          child: Text('Hello, Flutter!', style: TextStyle(fontSize: 24)),
+    return MaterialApp(
+      theme: _isDayMode ? ThemeData.light() : ThemeData.dark(),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Fading Text Animation'),
+          actions: [
+            IconButton(
+              onPressed: _toggleMode,
+              icon: Icon(_isDayMode ? Icons.mode_night : Icons.sunny),
+            ),
+          ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: toggleVisibility,
-        child: Icon(Icons.play_arrow),
+        body: Center(
+          child: AnimatedOpacity(
+            opacity: _isVisible ? 1.0 : 0.0,
+            duration: Duration(seconds: 1),
+            child: Text('Hello, Flutter!', style: TextStyle(fontSize: 24)),
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: toggleVisibility,
+          child: Icon(Icons.play_arrow),
+        ),
       ),
     );
   }
